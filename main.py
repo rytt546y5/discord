b4829e4df85f1df5f507de5fa0ff7dc657465ab1
rytt546y5 <qianchuanyeyue1@gmail.com>
2026-06-18 08:54:57 +0900

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import os
+import traceback
+from dotenv import load_dotenv
+from datetime import datetime, timezone
+load_dotenv()
+
+# ★envファイルからBOTのトークンを読み込む
+token = os.getenv('TOKEN')
+
+# BOTのステータスを設定
+STATUS = "Createby:@keru_developer_"
+
+# BOTフロッターを設定(とりあえず自分の名前)
+FOOTER_TEXT = "Createby:@keru_developer_"
+
+
+
+# ここからしたはいじらない
+# ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='$', intents=intents, help_command=None)
+
+async def load_cogs():
+
+    bot.embed_footer = FOOTER_TEXT
+
+    for filename in os.listdir("./Cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"Cogs.{filename[:-3]}")
+
+    await bot.tree.sync()
+
+bot.setup_hook = load_cogs
+
+@bot.event
+async def on_ready():
+    print("起動に成功しました👍🏻.")
+    await bot.change_presence(activity=discord.Game(name=STATUS), status=discord.Status.idle)
+
+@bot.tree.error
+async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+    if isinstance(error, app_commands.CheckFailure):
+        print(f"{interaction.user}によるコマンド({interaction.command.name})の実行がブロックされました。")
+        return
+    print(error)
+    traceback.print_exc()
+
+bot.run(token)

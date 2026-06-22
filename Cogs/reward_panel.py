b3532adff2a2127992eb41de93b0b1372aff10b2
rytt546y5 <qianchuanyeyue1@gmail.com>
2026-06-22 18:07:03 +0900

    reward_panel.py

--- Cogs/reward_panel.py
+++ Cogs/reward_panel.py
@@ -195,7 +195,7 @@ class ItemSelectView(discord.ui.View):
         self.add_item(
             ItemSelect(guild_id)
         )
-        # =====================
+# =====================
 # PANEL VIEW
 # =====================
 class RewardPanelView(discord.ui.View):
